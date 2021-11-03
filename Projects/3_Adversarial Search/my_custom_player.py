8c637ccc147a37dbf705a2354d827a8e6c2ccb60
Robert Paech <robert.a.paech@leidos.com>
2021-11-03 12:39:37 +1100

    Implemented negamax with AB pruning.

--- Projects/3_Adversarial Search/my_custom_player.py
+++ Projects/3_Adversarial Search/my_custom_player.py
@@ -1,43 +1,57 @@
 import random
 from sample_players import DataPlayer
+from isolation.isolation import Action, Isolation
+from typing import Tuple
+
+
+MAX_SEARCH_DEPTH = 2
 
 
 class CustomPlayer(DataPlayer):
 
-    def get_action(self, state):
+
+    def get_action(self, state: Isolation) -> None:
+
         if state.ply_count < 2:
             self.queue.put(random.choice(state.actions()))
         else:
-            self.queue.put(self.search(state, depth=4))
+            _, action = self.search(state, 0, float('-inf'), float('inf'))
+            self.queue.put(action)
 
-    def search(self, state, depth):
 
-        def min_value(state, depth):
-            if state.terminal_test(): 
-                return state.utility(self.player_id)
-            if depth <= 0: 
-                return self.score(state)
-            value = float("inf")
-            for action in state.actions():
-                value = min(value, max_value(state.result(action), depth - 1))
-            return value
+    def search(self, state: Isolation, depth: int, 
+               alpha: float, beta: float) -> Tuple[float, Action]:
 
-        def max_value(state, depth):
-            if state.terminal_test(): 
-                return state.utility(self.player_id)
-            if depth <= 0: 
-                return self.score(state)
-            value = float("-inf")
-            for action in state.actions():
-                value = max(value, min_value(state.result(action), depth - 1))
-            return value
+        if state.terminal_test() or depth == MAX_SEARCH_DEPTH:
+            return self.score(state), None
 
-        return max(state.actions(), 
-                   key=lambda x: min_value(state.result(x), depth - 1))
+        best_action = None
+        best_score = float('-inf')
 
-    def score(self, state):
+        for action in state.actions():
+
+            new_state = state.result(action)
+
+            score, _ = self.search(new_state, depth + 1, 
+                                   -beta, -max(alpha, best_score))
+            score = -score
+
+            if score > best_score:
+                best_score = score
+                best_action = action
+
+            if best_score >= beta:
+                break
+
+        return best_score, best_action
+
+
+    def score(self, state: Isolation) -> int:
+
         own_loc = state.locs[self.player_id]
         opp_loc = state.locs[1 - self.player_id]
+
         own_liberties = state.liberties(own_loc)
         opp_liberties = state.liberties(opp_loc)
+
         return len(own_liberties) - len(opp_liberties)
