be15a6b36f96ca7861da13173ecf3fa514de4162
Robert Paech <robert.a.paech@leidos.com>
2021-11-04 09:30:21 +1100

    Extracted custom players into separate python files.

--- Projects/3_Adversarial Search/custom_minimax_player.py
+++ Projects/3_Adversarial Search/custom_minimax_player.py
@@ -1,38 +1,51 @@
 import random
 from sample_players import DataPlayer
+from isolation.isolation import Action, Isolation
+from typing import Tuple
 
 
-class CustomPlayer(DataPlayer):
+MAX_SEARCH_DEPTH = 3
+
+
+class CustomMinimaxPlayer(DataPlayer):
 
     def get_action(self, state):
         if state.ply_count < 2:
             self.queue.put(random.choice(state.actions()))
         else:
-            self.queue.put(self.search(state, depth=3))
+            self.queue.put(self.search(state, depth=MAX_SEARCH_DEPTH))
 
     def search(self, state, depth):
 
         def min_value(state, depth):
-            if state.terminal_test(): 
+
+            if state.terminal_test():
                 return state.utility(self.player_id)
-            if depth <= 0: 
+
+            if depth <= 0:
                 return self.score(state)
+
             value = float("inf")
             for action in state.actions():
                 value = min(value, max_value(state.result(action), depth - 1))
+
             return value
 
         def max_value(state, depth):
-            if state.terminal_test(): 
+
+            if state.terminal_test():
                 return state.utility(self.player_id)
-            if depth <= 0: 
+
+            if depth <= 0:
                 return self.score(state)
+
             value = float("-inf")
             for action in state.actions():
                 value = max(value, min_value(state.result(action), depth - 1))
+
             return value
 
-        return max(state.actions(), 
+        return max(state.actions(),
                    key=lambda x: min_value(state.result(x), depth - 1))
 
     def score(self, state):
@@ -40,4 +53,5 @@ class CustomPlayer(DataPlayer):
         opp_loc = state.locs[1 - self.player_id]
         own_liberties = state.liberties(own_loc)
         opp_liberties = state.liberties(opp_loc)
-        return len(own_liberties) - len(opp_liberties)
+        result = len(own_liberties) - len(opp_liberties)
+        return result
---
+++ Projects/3_Adversarial Search/custom_negamax_player.py
@@ -0,0 +1,58 @@
+import random
+from sample_players import DataPlayer
+from isolation.isolation import Action, Isolation
+from typing import Optional, Tuple
+
+
+MAX_SEARCH_DEPTH = 3
+
+
+class CustomNegamaxPlayer(DataPlayer):
+
+    def get_action(self, state: Isolation) -> None:
+
+        if state.ply_count < 2:
+            self.queue.put(random.choice(state.actions()))
+        else:
+            _, action = self.search(state)
+            self.queue.put(action)
+
+    def search(self, state: Isolation, depth: int = 0
+               ) -> Tuple[float, Optional[Action]]:
+
+        if state.terminal_test():
+            actions = state.actions()
+            if len(actions) > 0:
+                return state.utility(state.player()), actions[0]
+            else:
+                return state.utility(state.player()), None
+
+        if depth == MAX_SEARCH_DEPTH:
+            return self.score(state), None
+
+        best_action = None
+        best_score = float('-inf')
+
+        for action in state.actions():
+
+            new_state = state.result(action)
+
+            score, _ = self.search(new_state, depth + 1)
+            score = -score
+
+            if score > best_score:
+                best_score = score
+                best_action = action
+
+        return best_score, best_action
+
+    def score(self, state: Isolation) -> int:
+
+        own_loc = state.locs[state.player()]
+        opp_loc = state.locs[1 - state.player()]
+
+        own_liberties = state.liberties(own_loc)
+        opp_liberties = state.liberties(opp_loc)
+
+        result = len(own_liberties) - len(opp_liberties)
+        return result
